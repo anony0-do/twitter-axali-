365b9051d6ade5e9801e7a1a422f329bab4541cd
anony0-do <marikvara12@gmail.com>
2024-05-15 16:16:29 +0400

    gamarjoba

---
+++ styile.js
@@ -0,0 +1,16 @@
+let buttons = document.querySelectorAll('.butt');
+
+buttons.forEach(function(button) {
+    button.style.backgroundColor = 'darkblue';
+    button.addEventListener('mouseout', mouseOut);
+});
+
+function id1() {
+    buttons.forEach(function(button) {
+        button.style.backgroundColor = 'red';
+    });
+}
+
+function mouseOut() {
+    this.style.backgroundColor = 'darkblue'; // Access style of individual button
+}
